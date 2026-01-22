e78ef676082f6929e16ebc0c10a6c412efb1b864
extrzzzz <lomaniinoss@yandex.ru>
2026-01-22 21:24:57 +0300

    ci fix

--- src/test/java/com/company/bikerent/integration/BaseIntegrationTest.java
+++ src/test/java/com/company/bikerent/integration/BaseIntegrationTest.java
@@ -26,6 +26,7 @@ public abstract class BaseIntegrationTest {
         registry.add("spring.datasource.url", postgres::getJdbcUrl);
         registry.add("spring.datasource.username", postgres::getUsername);
         registry.add("spring.datasource.password", postgres::getPassword);
+        registry.add("spring.datasource.driver-class-name", () -> "org.postgresql.Driver");
         registry.add("spring.jpa.hibernate.ddl-auto", () -> "create-drop");
     }
 }
